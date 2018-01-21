9c823641275f381869257cc8199d69df343847b3
arminakvn <armin.akhavan@gmail.com>
2018-01-20 21:55:08 -0500

    new look

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function initSlider3 (_data, f ,slid_item) {
   const svg = d3.select('body').append('svg')
       .attr('height', 500)
       .attr('width', 500)
-      
+    
     const mySlider = slid3r()
       .width(200)
       .range([-10,10])
@@ -17,8 +17,9 @@ function initSlider3 (_data, f ,slid_item) {
       .label('Supply')
       .numTicks(21)
       .loc([50, 50])
-      .onDrag((pos) => d3.select('#sliderValue').text(pos))
+      .onDrag((pos) => console.log(pos))
       .onDone(function(pos){
+        var new_data = []
          var lookup_change = slid_item.filter(function(d){
 
           return d["tick"] == (pos).toString();
@@ -29,13 +30,14 @@ function initSlider3 (_data, f ,slid_item) {
       d = _data[i]
       console.log(d["Year"],lookup_change)
      console.log( lookup_change[0][d["Year"]])
-     d["Supply"] = d["Supply"] + +lookup_change[0][d["Year"]]
-
+     var new_supl = d["Supply"] + +lookup_change[0][d["Year"]]*1000
+      var d_2 = {"Demand": d["Demand"], "Year": d["Year"], "date": d["date"], "Supply": new_supl}
+      new_data.push(d_2)
 
   }
 
 
-         updateData(pos,_data,lookup_change);
+         updateData(pos,new_data,lookup_change);
         });
   
     svg.append('g').call(mySlider);
@@ -45,81 +47,55 @@ function initSlider3 (_data, f ,slid_item) {
 
 
 
-function initViz3 (_data) {
-  var svg = d3.select("#chart_1").append("svg");
-  svg.attr('width', c_w);
-  svg.attr('height', c_h);
-  var margin = {top: 20, right: 20, bottom: 30, left: 50},
-    width = +svg.attr("width") - margin.left - margin.right,
-    height = +svg.attr("height") - margin.top - margin.bottom,
-    g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+function initViz3 (_data,j_slid) {
 
-var parseTime = d3.timeParse("%d-%b-%y");
- x = d3.scaleTime()
-    .rangeRound([0, width]);
- y = d3.scaleLinear()
-    .rangeRound([height, 0]);
-
-
-
-
-     xAxis = d3.axisBottom(x);
-
- yAxis = d3.axisLeft(y);
-
-
-
-
-
- area = d3.area()
-    .x(function(d) { return x(d.date); })
-    .y1(function(d) { return y(d["Supply"]); });
-
- line = d3.line()
-    // .interpolate("basis")
-    .x(function(d) { return x(d.date); })
-    .y(function(d) { return y(d["Supply"]); })
-    .curve(d3.curveCatmullRomOpen)
-    // .curve(d3.curveStep)
-    // .context(context);
-// d3.tsv("data.tsv", function(d) {
-//   d.date = parseTime(d.date);
-//   d.close = +d.close;
-//   return d;
-// }, function(error, data) {
-//   if (error) throw error;
-
-  x.domain(d3.extent(_data, function(d) { return d.date; }));
-  y.domain([d3.min(_data, function(d) { return d["Supply"]; }), d3.max(_data, function(d) { return d["Supply"]; })]);
   
-g.append("path")
-  .data([_data])
-      .attr("class", "line")
-      .attr("d", line);
+  
+  x.domain(d3.extent(_data, function(d) { return d.date; }));
+  // y.domain([d3.min(_data, function(d) { return d["Supply"]; }), d3.max(_data, function(d) { return d["Supply"]; })]);
+  y.domain([0, d3.max(_data, function(d) { return d["Supply"]; })+2500000]);
+  
+  g.append("path")
+    .data([_data])
+        .attr("class", "line")
+        .attr("d", line);
 
-  area.y0(y(d3.min(_data, function(d) { return d["Supply"]; })));
+  // area.y0(y(d3.min(_data, function(d) { return d["Supply"]; })));
 
-  // g.append("path")
-  //     .datum(_data)
-  //     .attr("fill", "steelblue")
-  //     .attr("d", area);
+  area.y0(y(0));
+
+  g.append("path")
+      .data([_data])
+      .attr("fill", "steelblue")
+      .attr("class", "area")
+      .attr("d", area);
   
 
 
 
   g.append("g")
       .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(x));
+      .call(xAxis);
 
   g.append("g")
-      .call(d3.axisLeft(y))
+      .call(yAxis)
+      .attr("class","yaxistext")
     .append("text")
+    
       .attr("fill", "#000")
       .attr("transform", "rotate(-90)")
       .attr("y", 6)
       .attr("dy", "0.71em")
       .attr("text-anchor", "end")
       .text("");
+
+
+
+
+    initSlider3(_data, ["Supply"],j_slid);
+
+
+
 // });
 }
 
@@ -186,10 +162,10 @@ function dataDidLoad(error, j, j_slid, k) { //add topics if necessary
         
       // })
 
-      console.log(_data)
-        var _nestedData = d3.nest()
-          .key(function(d){return d["Year"]})
-          .entries(_data);
+      console.log("_data",_data)
+        // var _nestedData = d3.nest()
+          // .key(function(d){return d["Year"]})
+          // .entries(_data);
 
 
       // console.log(_nestedData)
@@ -246,10 +222,9 @@ state_of_viz.set('_data', _data);
 
 
 
-      initViz3(_data);
+      initViz3(_data,j_slid);
 
 
-initSlider3(_data, ["Supply"],j_slid);
 }
 
 
@@ -263,27 +238,59 @@ state_of_viz = d3.map()
 
   var c_w = 960;
   var c_h = 500;
-  var x, y,xAxis,yAxis, line, area, _data;
+  // var x, y,xAxis,yAxis, line, area, _data;
 
   
   state_of_viz.set('drag_location', 0);
 
 
+var svg = d3.select("#chart_1").append("svg");
+  svg.attr('width', c_w);
+  svg.attr('height', c_h);
+  var margin = {top: 20, right: 20, bottom: 30, left: 150},
+    width = +svg.attr("width") - margin.left - margin.right,
+    height = +svg.attr("height") - margin.top - margin.bottom,
+    g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
+var parseTime = d3.timeParse("%d-%b-%y");
+var x = d3.scaleTime()
+    .rangeRound([0, width]);
+var y = d3.scaleLinear()
+    .rangeRound([height, 0]);
+
+
+
+  var xAxis = d3.axisBottom(x);
+
+  var yAxis = d3.axisLeft(y);
+
+
+
+
+
+ var area = d3.area()
+    .x(function(d) { return x(d.date); })
+    .y1(function(d) { return y(d["Supply"]); });
+
+ var line = d3.line()
+    // .interpolate("basis")
+    .x(function(d) { return x(d.date); })
+    .y(function(d) { return y(d["Supply"]); })
+    // .curve(d3.curveCatmullRomOpen)
 
 var q = d3.queue(
-  ).defer(d3.csv, "https://arminakvn.github.io/cxoviz/graph1-state0.csv"
+  ).defer(d3.csv, "graph1-state0.csv"
   // loading the slider table / data
-  ).defer(d3.csv, "https://arminakvn.github.io/cxoviz/graph1-slider1-higher_ed.csv"
-  ).defer(d3.csv, "https://arminakvn.github.io/cxoviz/chart2-data-security-jobs.csv");
+  ).defer(d3.csv, "graph1-slider1-higher_edit.2018-0120.csv"
+  ).defer(d3.csv, "chart2-data-security-jobs.csv");
 q.await(dataDidLoad);
 
 
 
 
-function updateData(pos,_data,lookup_change) {
+function updateData(pos,new_data,lookup_change) {
 
-  console.log(pos,lookup_change,_data)
+  console.log(pos,lookup_change,new_data)
 
   
 
@@ -297,35 +304,57 @@ console.log(lookup_change)
    
 
 
-console.log(_data)
-    // Get the data again
-    // d3.csv("data-alt.csv", function(error, data) {
-
-      //   data.forEach(function(d) {
-      //   d.date = parseDate(d.date);
-      //   d.close = +d.close;
-      // });
+console.log(new_data)
+   
 
       // Scale the range of the data again 
-      x.domain(d3.extent(_data, function(d) { return d.date; }));
-      y.domain([0, d3.max(_data, function(d) { return d["Supply"]; })]);
-      // console.log(x,y)
-    // Select the section we want to apply our changes to
-    var svg = d3.select("svg").transition();
-    console.log(svg.select("path.line") )
-    // Make the changes
-        svg.select(".line") 
-        .data(_data)
-            // .attr("d", line(_data))
-            .duration(70)
-            .attr("d", line);
-        svg.select(".x.axis") // change the x axis
-            .duration(70)
-            .call(xAxis);
-        svg.select(".y.axis") // change the y axis
-            .duration(70)
-            .call(yAxis);
+      // x.domain(d3.extent(new_data, function(d) { return d.date; }));
+      // y.domain([0, d3.max(new_data, function(d) { return d["Supply"]; })]);
+      // var xAxis = d3.axisBottom(x);
 
+      //   var yAxis = d3.axisLeft(y);
+
+
+
+
+
+    // Make the changes
+
+
+
+        g.select("path.line") 
+        .datum(new_data)
+            // .attr("d", line(_data))
+            // .duration(70)
+            .attr("d", line(new_data));
+
+
+             g.select("path.area") 
+        .datum(new_data)
+            // .attr("d", line(_data))
+            // .duration(70)
+            .attr("d", area(new_data));
+
+
+
+
+//
+
+        // g.select(".x.axis") // change the x axis
+        //     // .duration(70)
+        //     .call(xAxis);
+        // g.select(".y.axis") // change the y axis
+        //     // .duration(70)
+        //     .call(yAxis);
+
+
+        // g.select(".yaxistext")
+        // .call(yAxis)
+
+
+
+
+//
     // });
 }
 
